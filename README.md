a496eb7e498bb01f662a98979aa8934188659317
Oleksandr Grytsenko <grytole@gmail.com>
2016-03-08 19:38:55 +0200

    Update protocol to make commands more universal

--- README.md
+++ README.md
@@ -18,34 +18,60 @@ Hardware is based on ready modules: usb-uart bridge and microcontroller developm
 
 All actions are based on transmitting a command followed by required number of argumens. Every request is followed by success or failure response.
 
-Response is unified. First byte is a prefix with number of following bytes. `00h` means that failure happened during command execution and payload size is zero, what means it is not followed by another data related to this request. Successful command execution has size of response payload followed by argument of request.
+Response is unified. First byte is a prefix with number of following bytes. `00h` means that failure happened during command execution and payload size is zero, what means it is not followed by another data related to this request. Successful command execution has size of response payload followed by set of bytes with response.
 
 ##### List of commands
 
- - **Write byte** - `[00h] [address] [register] [byte]`
+ - **Write** - `00h, address, register, numbytes, byte1, [..., byteN]` -> `numbytes, [byte1, ..., byteN]`
  
-   TX: `00h 77h F4h 2Eh` - write byte `2Eh` into register `F4h` of device with address `77h`
+ - **Read** - `01h, address, register, numbytes` -> `numbytes, [byte1, ..., byteN]`
 
-   RX: `01h 2Eh` - write success, response has one byte with value equal to transmitted
+ - **Detect devices** - `02h, numbytes, [address1, ..., addressN]` -> `numbytes, [address1, ..., addressN]`
 
-   RX: `00h` - write failure, operation was not acknowledged
+##### Examples
 
- - **Read byte** - `[01h] [address] [register]`
-
-   TX: `01h 1Eh 0Ah` - read byte from register `0Ah` of device with address `1Eh`
-
-   RX: `01h 48h` - read success, response has one byte with value `48h`
-
-   RX: `00h` - read failure, operation was not acknowledged
-
- - **Detect device** - `[02h] [address]`
-
-   TX: `02h 69h` - detect presence of device with address `69h` on the bus
-
-   RX: `01h 69h` - detect success, response has one byte with value of device address
-
-   RX: `00h` - detect failure, no device with requested address found
-
+ - write one byte `2Eh` into register `F4h` of device with address `77h`
+ 
+   TX: `00h 77h F4h 01h 2Eh` - request a sample of temperature value from BMP180 barometer
+   
+   RX: `01h 2Eh` - success, request sent
+   
+ - write two bytes `F0h 0Fh` into register `64h` of device with address `60h`
+ 
+   TX: `00h 60h 64h 02h F0h 0Fh` - convert digit `F0h 0Fh` to analog value on MCP4725 DAC device
+   
+   RX: `00h` - failure, MCP4725 DAC device may be not connected on the bus
+   
+ - read three bytes from register `0Ah` of device with address `1Eh`
+ 
+   TX: `01h 1Eh 0Ah 03h` - read chip ID value of HMC5883L compass
+   
+   RX: `03h 48h 34h 33h` - success, three bytes of chip ID received
+   
+ - read single byte from register `FEh` of device with address `40h` which is disconnected from the bus
+ 
+   TX: `01h 40h FEh 01h` - read prescaler value of PCA9685 PWM controller
+   
+   RX: `00h` - failure, PCA9685 PWM controller may be not connected on the bus
+   
+ - detect presence of device with address `69h` on the bus
+ 
+   TX: `02h 01h 69h` - check that MPU6050 IMU is on the bus
+   
+   RX: `01h 69h` - device was found
+   
+ - detect all present devices on the bus
+ 
+   TX: `02h 00h` - scan all valid range of addresses for presence (`03h, ..., 77h`)
+   
+   RX: `03h 1Eh 69h 77h` - three devices was detected: HMC5883L compass, MPU6050 IMU and BMP180 barometer
+   
+ - detect presence of device with address `40h` on the bus
+ 
+   TX: `02h 01h 40h` - check that PCA9685 PWM controller is on the bus
+   
+   RX: `00h` - no device with address `40h` was found
+   
 ---
 
 #### Software design
--- src/main.c
+++ src/main.c
@@ -1,25 +1,27 @@
-// usb-i2c-adapter / 06-Mar-2016 / grytole@gmail.com
+// usb-i2c-adapter / 08-Mar-2016 / grytole@gmail.com
 
 // Write byte to I2C device (command ID = 00h):
-//  TX:         [00h] [add] [reg] [val]
-//  RX SUCCESS: [01h] [val]
-//  RX FAILURE: [00h]
+//  TX:         00h, address, register, numbytes, byte1, [..., byteN]
+//  RX SUCCESS: numbytes, byte1, [..., byteN]
+//  RX FAILURE: 00h
 
 // Read byte from I2C device (command ID = 01h):
-//  TX:         [01h] [add] [reg]
-//  RX SUCCESS: [01h] [val]
-//  RX FAILURE: [00h]
+//  TX:         01h, address, register, numbytes
+//  RX SUCCESS: numbytes, byte1, [..., byteN]
+//  RX FAILURE: 00h
 
-// Detect presence of I2C device (command ID = 02h):
-//  TX:         [01h] [add]
-//  RX SUCCESS: [01h] [add]
-//  RX FAILURE: [00h]
+// Detect presence of I2C devices (command ID = 02h):
+//  TX:         02h, numbytes, [address1, ..., addressN]
+//  RX SUCCESS: numbytes, address1, [..., addressN]
+//  RX FAILURE: 00h
 
 #include "stm8s.h"
 
 #define DEFAULT_UART_BAUDRATE (115200)
+#define MAX_PAYLOAD_SIZE (255)
+#define MIN_DETECT_ADDRESS (0x03)
+#define MAX_DETECT_ADDRESS (0x77)
 #define RESPOND_FAILURE (0x00)
-#define RESPOND_SUCCESS (0x01)
 
 enum {
     STATE_STARTUP = 0,
@@ -36,11 +38,11 @@ enum {
 
 static struct {
     uint8_t state;
-    uint8_t cmd;
-    uint8_t add;
-    uint8_t reg;
-    uint8_t val;
-    uint8_t err;
+    uint8_t command;
+    uint8_t deviceAddress;
+    uint8_t deviceRegister;
+    uint8_t payloadSize;
+    uint8_t payload[ MAX_PAYLOAD_SIZE ];
 } adapter = {
     .state = STATE_STARTUP
 };
@@ -49,35 +51,57 @@ static void adapterInit( void )
 {
     CLK_HSIPrescalerConfig( CLK_PRESCALER_HSIDIV1 );
     UART1_DeInit();
-    UART1_Init( DEFAULT_UART_BAUDRATE, UART1_WORDLENGTH_8D, UART1_STOPBITS_1, UART1_PARITY_NO, UART1_SYNCMODE_CLOCK_DISABLE, UART1_MODE_TXRX_ENABLE );
+    UART1_Init( DEFAULT_UART_BAUDRATE,
+                UART1_WORDLENGTH_8D,
+                UART1_STOPBITS_1,
+                UART1_PARITY_NO,
+                UART1_SYNCMODE_CLOCK_DISABLE,
+                UART1_MODE_TXRX_ENABLE );
     I2C_DeInit();
-    I2C_Init( I2C_MAX_STANDARD_FREQ, 0x0000, I2C_DUTYCYCLE_2, I2C_ACK_CURR, I2C_ADDMODE_7BIT, I2C_MAX_INPUT_FREQ );
+    I2C_Init( I2C_MAX_STANDARD_FREQ,
+              0x0000,
+              I2C_DUTYCYCLE_2,
+              I2C_ACK_CURR,
+              I2C_ADDMODE_7BIT,
+              I2C_MAX_INPUT_FREQ );
     adapter.state = STATE_RECEIVE;
 }
 
 static void adapterReceive( void )
 {
     while( RESET == UART1_GetFlagStatus( UART1_FLAG_RXNE ) );
-    adapter.cmd = UART1_ReceiveData8();
-    switch( adapter.cmd )
+    adapter.command = UART1_ReceiveData8();
+    switch( adapter.command )
     {
     case COMMAND_WRITE:
         while( RESET == UART1_GetFlagStatus( UART1_FLAG_RXNE ) );
-        adapter.add = UART1_ReceiveData8();
+        adapter.deviceAddress = UART1_ReceiveData8();
         while( RESET == UART1_GetFlagStatus( UART1_FLAG_RXNE ) );
-        adapter.reg = UART1_ReceiveData8();
+        adapter.deviceRegister = UART1_ReceiveData8();
         while( RESET == UART1_GetFlagStatus( UART1_FLAG_RXNE ) );
-        adapter.val = UART1_ReceiveData8();
+        adapter.payloadSize = UART1_ReceiveData8();
+        for( uint8_t i = 0; i < adapter.payloadSize; i++ )
+        {
+            while( RESET == UART1_GetFlagStatus( UART1_FLAG_RXNE ) );
+            adapter.payload[ i ] = UART1_ReceiveData8();
+        }
         break;
     case COMMAND_READ:
         while( RESET == UART1_GetFlagStatus( UART1_FLAG_RXNE ) );
-        adapter.add = UART1_ReceiveData8();
+        adapter.deviceAddress = UART1_ReceiveData8();
         while( RESET == UART1_GetFlagStatus( UART1_FLAG_RXNE ) );
-        adapter.reg = UART1_ReceiveData8();
+        adapter.deviceRegister = UART1_ReceiveData8();
+        while( RESET == UART1_GetFlagStatus( UART1_FLAG_RXNE ) );
+        adapter.payloadSize = UART1_ReceiveData8();
         break;
     case COMMAND_DETECT:
         while( RESET == UART1_GetFlagStatus( UART1_FLAG_RXNE ) );
-        adapter.add = UART1_ReceiveData8();
+        adapter.payloadSize = UART1_ReceiveData8();
+        for( uint8_t i = 0; i < adapter.payloadSize; i++ )
+        {
+            while( RESET == UART1_GetFlagStatus( UART1_FLAG_RXNE ) );
+            adapter.payload[ i ] = UART1_ReceiveData8();
+        }
         break;
     default:
         break;
@@ -87,82 +111,102 @@ static void adapterReceive( void )
 
 static void adapterExecute( void )
 {
-    switch( adapter.cmd )
+    switch( adapter.command )
     {
     case COMMAND_WRITE:
         while( RESET != I2C_GetFlagStatus( I2C_FLAG_BUSBUSY ) );
         I2C_GenerateSTART( ENABLE );
         while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_MODE_SELECT ) );
-        I2C_Send7bitAddress( adapter.add << 1, I2C_DIRECTION_TX );
+        I2C_Send7bitAddress( adapter.deviceAddress << 1, I2C_DIRECTION_TX );
         while( ( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) ) &&
                ( RESET == I2C_GetFlagStatus( I2C_FLAG_ACKNOWLEDGEFAILURE ) ) );
         if( RESET == I2C_GetFlagStatus( I2C_FLAG_ACKNOWLEDGEFAILURE ) )
         {
-            I2C_SendData( adapter.reg );
+            I2C_SendData( adapter.deviceRegister );
             while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_BYTE_TRANSMITTING ) );
-            I2C_SendData( adapter.val );
-            while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_BYTE_TRANSMITTING ) );
-            I2C_GenerateSTOP( ENABLE );
-            adapter.err = RESPOND_SUCCESS;
+            for( uint8_t i = 0; i < adapter.payloadSize; i++ )
+            {
+                I2C_SendData( adapter.payload[ i ] );
+                while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_BYTE_TRANSMITTING ) );
+                if( ( adapter.payloadSize - 1 ) == i )
+                {
+                    I2C_GenerateSTOP( ENABLE );
+                }
+            }
         }
         else
         {
             I2C_ClearFlag( I2C_FLAG_ACKNOWLEDGEFAILURE );
             I2C_GenerateSTOP( ENABLE );
-            adapter.err = RESPOND_FAILURE;
+            adapter.payloadSize = RESPOND_FAILURE;
         }
         break;
     case COMMAND_READ:
         while( RESET != I2C_GetFlagStatus( I2C_FLAG_BUSBUSY ) );
         I2C_GenerateSTART( ENABLE );
         while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_MODE_SELECT ) );
-        I2C_Send7bitAddress( adapter.add << 1, I2C_DIRECTION_TX );
+        I2C_Send7bitAddress( adapter.deviceAddress << 1, I2C_DIRECTION_TX );
         while( ( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) ) &&
                ( RESET == I2C_GetFlagStatus( I2C_FLAG_ACKNOWLEDGEFAILURE ) ) );
         if( RESET == I2C_GetFlagStatus( I2C_FLAG_ACKNOWLEDGEFAILURE ) )
         {
-            I2C_SendData( adapter.reg );
+            I2C_SendData( adapter.deviceRegister );
             while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_BYTE_TRANSMITTING ) );
             I2C_GenerateSTART( ENABLE );
             while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_MODE_SELECT ) );
-            I2C_Send7bitAddress( adapter.add << 1, I2C_DIRECTION_RX );
+            I2C_Send7bitAddress( adapter.deviceAddress << 1, I2C_DIRECTION_RX );
             while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED ) );
-            I2C_AcknowledgeConfig( I2C_ACK_NONE );
-            I2C_GenerateSTOP( ENABLE );
-            while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_BYTE_RECEIVED ) );
-            I2C_AcknowledgeConfig( I2C_ACK_CURR );
-            adapter.val = I2C_ReceiveData();
-            adapter.err = RESPOND_SUCCESS;
+            for( uint8_t i = 0; i < adapter.payloadSize; i++ )
+            {
+                if( ( adapter.payloadSize - 1 ) == i )
+                {
+                    I2C_AcknowledgeConfig( I2C_ACK_NONE );
+                }
+                while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_BYTE_RECEIVED ) );
+                adapter.payload[ i ] = I2C_ReceiveData();
+                if( ( adapter.payloadSize - 1 ) == i )
+                {
+                    I2C_GenerateSTOP( ENABLE );
+                    I2C_AcknowledgeConfig( I2C_ACK_CURR );
+                }
+            }
         }
         else
         {
             I2C_ClearFlag( I2C_FLAG_ACKNOWLEDGEFAILURE );
             I2C_GenerateSTOP( ENABLE );
-            adapter.err = RESPOND_FAILURE;
+            adapter.payloadSize = RESPOND_FAILURE;
         }
         break;
     case COMMAND_DETECT:
-        while( RESET != I2C_GetFlagStatus( I2C_FLAG_BUSBUSY ) );
-        I2C_GenerateSTART( ENABLE );
-        while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_MODE_SELECT ) );
-        I2C_Send7bitAddress( adapter.add << 1, I2C_DIRECTION_TX );
-        while( ( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) ) &&
-               ( RESET == I2C_GetFlagStatus( I2C_FLAG_ACKNOWLEDGEFAILURE ) ) );
-        if( RESET == I2C_GetFlagStatus( I2C_FLAG_ACKNOWLEDGEFAILURE ) )
+        if( 0 == adapter.payloadSize )
         {
-            adapter.val = adapter.add;
-            I2C_GenerateSTOP( ENABLE );
-            adapter.err = RESPOND_SUCCESS;
+            for( uint8_t i = MIN_DETECT_ADDRESS; i <= MAX_DETECT_ADDRESS; i++  )
+            {
+                adapter.payload[ adapter.payloadSize++ ] = i;
+            }
         }
-        else
+        for( uint8_t i = 0, j = 0, cnt = adapter.payloadSize; i < cnt; i++, adapter.payloadSize = j )
         {
-            I2C_ClearFlag( I2C_FLAG_ACKNOWLEDGEFAILURE );
+            while( RESET != I2C_GetFlagStatus( I2C_FLAG_BUSBUSY ) );
+            I2C_GenerateSTART( ENABLE );
+            while( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_MODE_SELECT ) );
+            I2C_Send7bitAddress( adapter.payload[ i ] << 1, I2C_DIRECTION_TX );
+            while( ( SUCCESS != I2C_CheckEvent( I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) ) &&
+                   ( RESET == I2C_GetFlagStatus( I2C_FLAG_ACKNOWLEDGEFAILURE ) ) );
+            if( RESET == I2C_GetFlagStatus( I2C_FLAG_ACKNOWLEDGEFAILURE ) )
+            {
+                adapter.payload[ j++ ] = adapter.payload[ i ];
+            }
+            else
+            {
+                I2C_ClearFlag( I2C_FLAG_ACKNOWLEDGEFAILURE );
+            }
             I2C_GenerateSTOP( ENABLE );
-            adapter.err = RESPOND_FAILURE;
         }
         break;
     default:
-        adapter.err = RESPOND_FAILURE;
+        adapter.payloadSize = RESPOND_FAILURE;
         break;
     }
     adapter.state = STATE_RESPOND;
@@ -171,11 +215,11 @@ static void adapterExecute( void )
 static void adapterRespond( void )
 {
     while( RESET == UART1_GetFlagStatus( UART1_FLAG_TXE ) );
-    UART1_SendData8( adapter.err );
-    if( RESPOND_FAILURE != adapter.err )
+    UART1_SendData8( adapter.payloadSize );
+    for( uint8_t i = 0; i < adapter.payloadSize; i++ )
     {
         while( RESET == UART1_GetFlagStatus( UART1_FLAG_TXE ) );
-        UART1_SendData8( adapter.val );
+        UART1_SendData8( adapter.payload[ i ] );
     }
     while( RESET == UART1_GetFlagStatus( UART1_FLAG_TC ) );
     adapter.state = STATE_RECEIVE;
